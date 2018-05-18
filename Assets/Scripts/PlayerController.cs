fd8d690a2e368c695759e16f066f7c929f4bc4f1
Matt Sparks <masparks@uci.edu>
2018-05-18 10:55:58 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float playerSpeed = 5f;
+	Rigidbody rb;
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	public float chargeTimer = 0;
+	private Vector3 moveDirection = Vector3.zero;
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+
+
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			chargeTimer += Time.deltaTime;
+			Debug.Log (chargeTimer.ToString ());
+		}
+		if (Input.GetKeyUp (KeyCode.Mouse0) && chargeTimer > 2) {
+			Debug.Log ("MOVE: " +  moveDirection.ToString ());
+//			if (moveDirection.x <= 0) {
+//				transform.forward *= 2;
+//			}
+//				controller.Move(new Vector3(10f, -1*gravity * Time.deltaTime,0f));
+//			else {
+				controller.Move (moveDirection * Time.deltaTime * chargeTimer * 3f);
+				chargeTimer = 0;
+
+		} else {
+			controller.Move (moveDirection * Time.deltaTime);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Repulse.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repulse : MonoBehaviour {
+	public float force = 1000f;
+	public Collider[] hits;
+	public float radius = 10f;
+
+	public float charge;
+	public float upModifier = 2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void toRepulse()
+	{
+		Vector3 pos = transform.position;
+		hits = Physics.OverlapSphere (transform.position, 2.5f);
+
+		foreach (Collider hit in hits)
+		{
+			Rigidbody rb = hit.GetComponent<Rigidbody> ();
+//			if (hit.gameObject.CompareTag("Explodeable"))
+//				hit.GetComponent<Rigidbody>().AddExplosionForce(force, pos , radius);
+
+			if (rb != null)
+				rb.AddExplosionForce(force, pos , radius, upModifier);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Q))
+			{
+			Debug.Log ("Q pressed");
+			toRepulse();
+		}
+
+			
+	}
+
+}
