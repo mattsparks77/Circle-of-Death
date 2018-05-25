9dd9ebd6cbb5b62ab0f1e5dde61ec95e8114e49e
TCWProgramming <34203476+TCWProgramming@users.noreply.github.com>
2018-05-25 15:46:39 -0700

    Final version
    Has respawns, bumpers, and the readme.  Speak now or forever hold your peace on any final changes.

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Bumper : NetworkBehaviour
+{
+
+    Vector3 bounceVelocity;
+    [Range(0.1f,50f)] public float bounceForce = 1f;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Collided");
+
+            bounceVelocity = new Vector3((transform.position.x - col.gameObject.transform.position.x) * bounceForce,
+                (transform.position.y - col.gameObject.transform.position.y) * bounceForce,
+                (transform.position.z - col.gameObject.transform.position.z) * bounceForce);
+            Debug.Log(bounceVelocity);
+            col.gameObject.GetComponent<Rigidbody>().AddForce(-bounceVelocity, ForceMode.VelocityChange);
+            col.gameObject.GetComponent<PlayerController>().StunPlayer(0.3f);
+        }
+    }
+}
